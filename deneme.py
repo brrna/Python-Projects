f39f0eee63ab9d4b4a121f99e72739ff75586089
brrna <bern.varol@gmail.com>
2023-02-16 01:27:35 +0300

    first modul

---
+++ deneme.py
@@ -0,0 +1,5 @@
+import modul1
+
+print(modul1.toplama(3, 6, 12))
+print(modul1.selamlama("Alien!!"))
+print(modul1.programlama_diller)
---
+++ modul1.py
@@ -0,0 +1,10 @@
+programlama_diller = ["Python", "Java", "php", "C++"]
+
+def toplama(a, b, c):
+    """
+    Bu fonksiyon üç sayının toplamını döndürür.
+    """
+    return a + b + c
+
+def selamlama(isim):
+    print("Heyyy", isim)
